766b320e37e33a387b667d69fb3e144bd6c760e5
Crystal Norman <cnorm88@gmail.com>
2020-10-11 15:56:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,8 @@ class Student < User
     @knowledge = []
   end
   
-  def learn(knowledge)
-    self.knowledge.join << knowledge
+  def learn(knowledge.join)
+    self.knowledge << knowledge
   end
 
 end
